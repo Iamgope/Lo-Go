88d8dd445dc1da3ece38ccfa773c2e60ceea87cc
iamgope <amangope005@gmail.com>
2021-03-15 18:27:13 +0530

    color adding is working properly

--- logomaker/views.py
+++ logomaker/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method == "POST":
+        name = str(request.POST.get('Cmpname'))
+        col = str(request.POST.get('Colname'))
+        
+        return render(request,'result.html',{'name':name,'col':col})
+
+    else:
+        return render(request,'index.html')
